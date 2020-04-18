c150379b945369f3918bfc9891c4133e5c76bd4f
Maya Benowitz <mayabenowitz@gmail.com>
2020-04-17 22:22:12 -0500

    added distance_correlation_matrix(), distance_correlation_network(), and updated HedgeFrame

--- hedgepy/core.py
+++ hedgepy/core.py
@@ -1,5 +1,8 @@
-from typing import Dict
+from typing import Dict, List
 import pandas as pd
+import dcor
+import networkx and nx
+from memoization import cached
 
 
 def ticker_time_frame(
@@ -24,7 +27,7 @@ def ticker_time_frame(
     return df_dict
 
 
-def detrend(frame: Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame]:
+def detrend_time_series(frame: Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame]:
     """detrends a bag of time series"""
 
     for time_series, df in frame.items():
@@ -32,23 +35,95 @@ def detrend(frame: Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame]:
 
     return frame
 
+@cached
+def distance_correlation_matrix(df):
+
+    tickers = df.columns.tolist()
+    df_dcor = pd.DataFrame(index=tickers, columns=tickers)
+
+    k=0
+    for i in tickers:
+        v_i = df.loc[:, i].values
+
+        for j in tickers[k:]:
+            v_j = df.loc[:, j].values
+            dcor_val = dcor.distance_correlation(v_i, v_j)
+            df_dcor.at[i, j] = dcor_val
+            df_dcor.at[j, i] = dcor_val
+
+        k+=1
+
+    return df_dcor
+
+@cached
+def distance_correlation_network(df, corr_threshold=0.33):
+
+    corr_matrix = df.values.astype('float')
+    sim_matrix = 1 - corr_matrix
+
+    G = nx.from_numpy_matrix(sim_matrix)
+    ticker_names = df.index.values
+
+    G = nx.relabel_nodes(G, lambda x: ticker_names[x])
+    G.edges(data=True)
+
+    H  = G.copy()
+
+    for (u, v, wt) in G.edges.data('weight'):
+        if wt >= 1 - corr_threshold:
+            H.remove_edge(u, v)
+
+        if u == v:
+            H.remove_edge(u, v)
+
+    return H
 
 class HedgeFrame(object):
     def __init__(self, df: pd.DataFrame, ticker_col_name: str, detrend: bool=True) -> None:
         self.df = df
         self.ticker_col_name = ticker_col_name
         self.detrend = detrend
-        self.preprocess()
 
-    def preprocess(self, detrend: bool=detrend) -> Dict[str, pd.DataFrame]:
-        frame = ticker_time_frame(self.df, ticker_col_name=self.ticker_col_name)
-
-        if detrend:
-            frame = detrend(frame)
-
-        keys = list(frame.keys())
+        if self.detrend:
+            frame = detrend_time_series(self.preprocess())
+        else:
+            frame = self.preprocess()
 
         self.frame = frame
+
+        def get_keys(frame: Dict[str, pd.DataFrame]) -> List[str]:
+            keys = list(frame.keys())
+            return keys
+
+        keys = get_keys(self.frame)
+        self.keys = keys
+
         self.first = frame[keys[0]]
         self.last = frame[keys[-1]]
+
+    def preprocess(self) -> Dict[str, pd.DataFrame]:
+        frame = ticker_time_frame(self.df, ticker_col_name=self.ticker_col_name)
         return frame
+
+    def dcor(self) -> Dict[str, pd.DataFrame]:
+
+        self.frame = {
+            time_series: distance_correlation_matrix(df) for time_series, df in self.frame.items()
+        }
+
+        self.first = self.frame[self.keys[0]]
+        self.last = self.frame[self.keys[-1]]
+
+        return self
+
+    def network(self, corr_threshold: int=0.33) -> Dict[str, pd.DataFrame]:
+
+        self.frame = {
+            time_series: distance_correlation_network(df_dcor, corr_threshold=corr_threshold)
+            for time_series, df_dcor in self.frame.items()
+        }
+
+        self.first = self.frame[self.keys[0]]
+        self.last = self.frame[self.keys[-1]]
+
+        return self
